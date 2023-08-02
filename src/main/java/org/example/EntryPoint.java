9b9dd92b665865d76157122fbdedd355c4a8e0f9
nicklagat <nicksonlagatk@gmail.com>
2023-08-02 17:52:03 +0300

    did some minor refactoring

--- src/main/java/org/example/EntryPoint.java
+++ src/main/java/org/example/EntryPoint.java
@@ -3,77 +3,75 @@ package org.example;
 import java.util.Arrays;
 
 public class EntryPoint {
-    // this is the entry point of our code execution
+    // This is the entry point of our code execution
     public static void main(String[] args) {
+        // Print a greeting message
         System.out.println("Hello Nick!!👏🏽");
 
-        // primitives
+        // Declare and initialize integer variables and print their sum
         int num1 = 30;
         int num2 = 11;
         int result = num1 + num2;
         System.out.println(result);
 
-        // double
-        double pie  = 3.14;
+        // Declare and print a double variable
+        double pie = 3.14;
         System.out.println(pie);
 
-        // boolean
+        // Declare and print a boolean variable
         boolean isAdult = false;
         System.out.println(isAdult);
 
-        // character
+        // Declare and print a character variable
         char a = 's';
         System.out.println(a);
 
-
+        // Declare and print an integer with underscores for readability
         int amount = 1_000_000;
         System.out.println(amount);
         System.out.println();
 
-
-
-
+        // Perform and print various arithmetic operations
         System.out.println(num1 + num2);
         System.out.println(num1 * num2);
         System.out.println(num1 / num2);
         System.out.println(num1 % num2);
 
-
-        int[] monthDays = {31,28,30,31};
-        System.out.println(Arrays.toString(monthDays));
+        // Declare and print an array of integers
+        int[] monthDays = {31, 28, 30, 31};
         System.out.println(Arrays.toString(monthDays));
         System.out.println("-------------------");
-        System.out.println(10+10);
 
+        // Print the sum of two integers
+        System.out.println(10 + 10);
 
+        // Increment and print an integer variable
         int test = 0;
         ++test;
         System.out.println(test);
 
-
+        // Create an object of the LocalVariable class and call its method
         LocalVariable localVariable = new LocalVariable();
         localVariable.method();
 
+        // Create objects of the InstanceVariable class and print one of them
         InstanceVariable instanceVariable = new InstanceVariable();
         InstanceVariable instanceVariable1 = new InstanceVariable("German Shepherd");
         System.out.println(instanceVariable1);
 
-
-        Dog mydog = new Dog("Bulldog","White",2,75.0);
+        // Create an object of the Dog class, modify its properties, and print it
+        Dog mydog = new Dog("Bulldog", "White", 2, 75.0);
         System.out.println(mydog);
         mydog.setAge(7);
         mydog.setColor("Black");
         System.out.println(mydog);
 
-        Car car = new Car("BMW","V7","Black Matte",2023,4,4,
-                "BMW Company Berlin",3600.00,1.00
-                );
+        // Create an object of the Car class, modify its properties, and print it
+        Car car = new Car("BMW", "V7", "Black Matte", 2023, 4, 4,
+                "BMW Company Berlin", 3600.00, 1.00
+        );
         car.setColor("Blue");
         car.setYear(2024);
         System.out.println(car);
-
     }
-
-
-
 }
--- src/main/java/org/example/Operators.java
+++ src/main/java/org/example/Operators.java
@@ -3,87 +3,79 @@ package org.example;
 public class Operators {
 
     public static void main(String[] args) {
+        // Declare and initialize variable a
         int a = 10;
 
-        if(a>5){
+        // If-else statement to check if a is greater than 5
+        if (a > 5) {
             System.out.println("a is greater than 5");
-        }
-
-        else {
+        } else {
             System.out.println("a is less than 5");
         }
 
-
+        // Switch statement to print the day of the week based on the value of day
         int day = 4;
-        switch(day){
+        switch (day) {
             case 1:
                 System.out.println("Monday");
                 break;
-
             case 2:
                 System.out.println("Tuesday");
                 break;
-
             case 3:
                 System.out.println("Wednesday");
                 break;
-
             case 4:
                 System.out.println("Thursday");
                 break;
-
         }
 
-        for(int i = 0; i<10; i++){
+        // For loop to print numbers from 0 to 9
+        for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
 
-
+        // While loop to print the age while it's less than 10
         int age = 3;
-        while(age < 10){
-            System.out.println("The age is: "+age);
+        while (age < 10) {
+            System.out.println("The age is: " + age);
             age++;
         }
 
-
+        // Nested for loop to print a pattern of asterisks
         for (int i = 0; i < 3; i++) { //outer loop
             for (int j = 0; j < 5; j++) { // inner loop
                 System.out.print("* ");
             }
             System.out.println();
-
         }
 
-
-        String[] colors = { "red", "green", "blue" };
+        // Nested for loop to print all pairs of colors
+        String[] colors = {"red", "green", "blue"};
         for (int i = 0; i < colors.length; i++) {
-            System.out.println(i);
             for (int j = i + 1; j < colors.length; j++) {
-                System.out.println(j);
                 System.out.println(colors[i] + ", " + colors[j]);
             }
         }
 
-        int [] numbers = {1,2,3,2,4,5,6,5,7};
-        for(int i = 0; i<numbers.length;i++){
-            System.out.println(i);
-            for(int j = i + 1; j<numbers.length;j++){
-                System.out.println("Printing J" + j);
-                if(numbers[i] == numbers[j]){
-                    System.out.println("Duplicate number found: " +numbers[i]);
+        // Nested for loop to find duplicate numbers in an array
+        int[] numbers = {1, 2, 3, 2, 4, 5, 6, 5, 7};
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j]) {
+                    System.out.println("Duplicate number found: " + numbers[i]);
                 }
             }
         }
 
-
+        // Bubble sort algorithm to sort an array in ascending order
         int[] randoms = {5, 4, 3, 1, 2, 7};
         for (int i = 0; i < randoms.length; i++) {
             for (int j = i + 1; j < randoms.length; j++) {
-                if(randoms[i]>randoms[j]){
-                    int temp =randoms[i];
+                if (randoms[i] > randoms[j]) {
+                    int temp = randoms[i];
                     randoms[i] = randoms[j];
                     randoms[j] = temp;
-
                 }
             }
         }
